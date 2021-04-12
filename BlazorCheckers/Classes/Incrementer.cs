00e4e5fbb0f18d4b09f3c161c3fc44dbe983485f
Byron <byronlong@gmail.com>
2021-04-12 18:30:03 -0400

    initial commit

---
+++ BlazorCheckers/Classes/Incrementer.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Classes
+{
+    public static class Incrementer
+    {
+        public static int CurrentCount = 0;
+
+        public static void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
